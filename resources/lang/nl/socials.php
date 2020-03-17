6e8eb9bfc54029dcd51a59980c7df4bb98d408b0
Jesse Berkhof <jesseberkhof95@gmail.com>
2020-03-17 22:57:28 +0100

    Added url

--- resources/lang/nl/socials.php
+++ resources/lang/nl/socials.php
@@ -2,15 +2,15 @@
 
 return [
     'facebook' => [
-        'message' => 'Doe een dab wanneer je moet niezen of hoesten om het corona virus tegen te gaan. %23CoronaDab Voor meer informatie:',
+        'message' => 'Doe een dab wanneer je moet niezen of hoesten om het corona virus tegen te gaan. %23CoronaDab Voor meer informatie: ' . config('app.url'),
     ],
     'twitter' => [
-        'message' => 'Doe een dab wanneer je moet niezen of hoesten om het corona virus tegen te gaan. %23CoronaDab Voor meer informatie:',
+        'message' => 'Doe een dab wanneer je moet niezen of hoesten om het corona virus tegen te gaan. %23CoronaDab Voor meer informatie: ' . config('app.url'),
     ],
     'linkedin' => [
-        'message' => 'Doe een dab wanneer je moet niezen of hoesten om het corona virus tegen te gaan. %23CoronaDab Voor meer informatie:',
+        'message' => 'Doe een dab wanneer je moet niezen of hoesten om het corona virus tegen te gaan. %23CoronaDab Voor meer informatie: ' . config('app.url'),
     ],
     'whatsapp' => [
-        'message' => 'Doe een dab wanneer je moet niezen of hoesten om het corona virus tegen te gaan. %23CoronaDab Voor meer informatie:',
+        'message' => 'Doe een dab wanneer je moet niezen of hoesten om het corona virus tegen te gaan. %23CoronaDab Voor meer informatie: ' . config('app.url'),
     ],
 ];
