17a210e49f6633b9279fa643f3fb1ed8181d2d40
Jesse Berkhof <jesseberkhof95@gmail.com>
2020-03-18 20:10:18 +0100

    fixes

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ Route::get(trans('routes.faq'), 'SiteController@faq')->name('faq');
 Route::get(trans('routes.thanks'), 'SiteController@thanks')->name('thanks');
 Route::get(trans('routes.donate'), 'SiteController@donate')->name('donate');
 
-Route::post('/thanks', 'AppreciationController@store')->name('thanks.store');
+Route::post(trans('routes.thanks'), 'AppreciationController@store')->name('thanks.store');
 
 Route::get('/debug-sentry', function () {
     throw new Exception('My first Sentry error!');
