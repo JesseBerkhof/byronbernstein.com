ab080403b3ec934112bc4f0f9ebf812c6de14b7b
Jesse Berkhof <jesseberkhof95@gmail.com>
2020-03-21 14:27:09 +0100

    responsive fixes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
         @include('components.header')
 
         <div class="flex items-center justify-center mt-24">
-            <div class="w-full sm:w-1/3 md:w-1/3 lg:w-1/3 xl:w-1/3 bg-white shadow p-8">
+            <div class="w-full sm:w-full md:w-1/2 lg:w-1/3 xl:w-1/3 bg-white shadow p-8">
                 <form class="w-full mb-4" action="{{ route('thanks.store') }}" method="POST">
                     @csrf
                     <div class="flex flex-wrap mb-6">
