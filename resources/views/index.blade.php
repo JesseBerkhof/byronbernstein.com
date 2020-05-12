9fef4350b800791ccf74f87a0528d4eafa7746c9
Jesse Berkhof <jesseberkhof95@gmail.com>
2020-05-12 18:49:52 +0200

    Removed ads

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -111,22 +111,6 @@
                 </div>
             </div>
             @endif
-            @if($index === 3)
-                <div class="flex flex-wrap justify-center">
-                    <div class="w-full sm:w-full md:w-full lg:w-1/2 xl:w-1/2 m-2 font-bold text-blue-900 text-center">
-                        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-                        <ins class="adsbygoogle"
-                             style="display:block"
-                             data-ad-format="fluid"
-                             data-ad-layout-key="-gw-3+1f-3d+2z"
-                             data-ad-client="ca-pub-6313840088307831"
-                             data-ad-slot="5273939724"></ins>
-                        <script>
-                             (adsbygoogle = window.adsbygoogle || []).push({});
-                        </script>
-                    </div>
-                </div>
-            @endif
 
             <div class="flex flex-wrap justify-center">
                 @foreach($chunk as $appreciation)
