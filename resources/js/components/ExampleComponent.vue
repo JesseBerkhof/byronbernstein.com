6cc750745806f6eadf5fffcde35e22b9f55f2f37
Jesse Berkhof <jesseberkhof95@gmail.com>
2020-05-27 21:19:28 +0200

    fixed untracked

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Example Component</div>
+
+                    <div class="card-body">
+                        I'm an example component.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    const sleep = (milliseconds) => {
+        return new Promise(resolve => setTimeout(resolve, milliseconds))
+    }
+
+    export default {
+        props: {
+          items: {}
+        },
+        methods() {
+            function sleep(ms) {
+                return new Promise(resolve => setTimeout(resolve, ms));
+            }
+        },
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
