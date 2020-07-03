cf0f79b606c7d6d559dbe9197ac7748847f758ec
Jesse Berkhof <jesseberkhof95@gmail.com>
2020-07-03 22:05:49 +0200

    Changed color

--- resources/views/components/message.blade.php
+++ resources/views/components/message.blade.php
@@ -1,9 +1,9 @@
 <div class="bg-gray-900 sm:rounded-lg m-2 {{ $unapproved === true ? 'border-2 border-purple-500' : '' }}">
     <div class="px-4 py-5 sm:p-6">
-        <h3 class="text-lg leading-6 font-medium text-gray-400">
+        <h3 class="text-lg leading-6 font-medium text-purple-400">
             {{ $message->username }}
         </h3>
-        <div class="mt-2 max-w-xl text-sm leading-5 text-gray-500">
+        <div class="mt-2 max-w-xl text-sm leading-5 text-gray-400">
             <p>
                 {{ $message->body }}
             </p>
