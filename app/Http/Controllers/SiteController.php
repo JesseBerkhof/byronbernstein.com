06ed61083e2a1b85d5fd5cb2f444d21a80724aa7
Jesse Berkhof <jesseberkhof95@gmail.com>
2020-07-04 15:34:23 +0200

    improved performance

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -9,17 +9,14 @@ class SiteController extends Controller
 {
      public function index() {
 
-         $messages = Message::query()->whereNotNull('approved_at')->orderByDesc('created_at')->paginate(100);
-         $unapproved = Message::query()->whereNull('approved_at')->orderByDesc('created_at')->paginate(100);
-
-         $hearts = Heart::all()->count();
-         $totalMessages = Message::all()->count();
+         $messages = Message::query()->whereNotNull('approved_at')->orderByDesc('created_at')->paginate(75);
+         $unapproved = Message::query()->whereNull('approved_at')->orderByDesc('created_at')->paginate(75);
 
          return view('index')->with([
-             'totalMessages' => $totalMessages,
-            'messages' => $messages,
-            'hearts' => $hearts,
-            'unapproved' => $unapproved
+             'totalMessages' => Message::count(),
+             'messages' => $messages,
+             'hearts' => Heart::count(),
+             'unapproved' => $unapproved
         ]);
      }
 }
