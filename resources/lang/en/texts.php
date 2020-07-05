50faff23b3d9ee0bb8e455df17b2f4d6ea1c6138
Jesse Berkhof <jesseberkhof95@gmail.com>
2020-07-05 19:24:04 +0200

    Changed name

--- resources/lang/en/texts.php
+++ resources/lang/en/texts.php
@@ -2,7 +2,7 @@
 
 return [
     'header' => [
-        'title' => 'R.I.P. <br> Byron "Reckful" Bernstein',
+        'title' => 'R.I.P. <br> Byron Daniel "Reckful" Bernstein',
         'subtitle' => 'May 8, 1989 - July 2, 2020'
     ],
     'feed_cta' => '',
