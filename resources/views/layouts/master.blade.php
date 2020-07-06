4fb0c993fec2188479fc7227ba4353e5e35d30cb
Jesse Berkhof <jesseberkhof95@gmail.com>
2020-07-06 17:26:05 +0200

    Added favicon.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -16,6 +16,10 @@
         <meta property="twitter:card" content="{{ asset('images/social.jpg') }}"/>
         <meta property="twitter:description" content="RIP Byron 'Reckful' Bernstein | May 8, 1989 - July 2, 2020"/>
         <meta property="twitter:image" content="{{ asset('images/social.jpg') }}"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/favicon/apple-touch-icon.png') }}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/favicon/favicon-32x32.png') }}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/favicon/favicon-16x16.png') }}">
+        <link rel="manifest" href="{{ asset('/favicon/site.webmanifest') }}">
         <title>{{ config('app.name') }}</title>
         <link href="{{ mix('css/app.css') }}" rel="stylesheet">
         <script src="https://unpkg.com/feather-icons"></script>
