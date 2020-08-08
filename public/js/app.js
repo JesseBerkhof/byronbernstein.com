d6452876cd05defd32ad333ebaa644417df88ad6
Jesse Berkhof <jesseberkhof95@gmail.com>
2020-08-08 14:16:43 +0200

    Fixed url

--- public/js/app.js
+++ public/js/app.js
@@ -15301,8 +15301,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/arctic/code/byronbernstein.com/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/arctic/code/byronbernstein.com/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/arctic/code/byron/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/arctic/code/byron/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
