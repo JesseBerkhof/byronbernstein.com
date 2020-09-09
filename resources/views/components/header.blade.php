dd501fd8550b62f3df0eefbb956d9c99f9a8700b
Jesse Berkhof <jesseberkhof95@gmail.com>
2020-09-09 19:03:06 +0200

    Fixed mobile button.

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,7 +1,7 @@
 <div class="relative pt-2 md:pb-2 lg:pb-4 xl:pb-8">
     <div class="max-w-screen-xl mx-auto px-4 sm:px-6">
         <nav class="relative flex items-center justify-between sm:h-10 md:justify-center">
-            <div class="hidden md:flex md:space-x-10">
+            <div class="mx-auto md:flex md:space-x-10">
                 <a href="{{ route('petitions.index') }}" class="font-medium text-purple-500 hover:text-purple-900 transition duration-150 ease-in-out">Click here to sign the reckH emote petition</a>
             </div>
         </nav>
