0f7c54d57cb15e60fcb9ade42627fdfe1de758cc
Jesse Berkhof <jesseberkhof95@gmail.com>
2020-09-09 23:16:01 +0200

    Refactored routes

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,26 @@
 <?php
 
+use App\Http\Controllers\BlacklistController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MessageController;
+use App\Http\Controllers\PetitionController;
+use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-Route::get('/', 'SiteController@index')->name('index');
+Route::get('/', [SiteController::class, 'index'])->name('index');
+Route::post('/message', [MessageController::class, 'store'])->name('thanks.store');
 
-Route::post('/message', 'MessageController@store')->name('thanks.store');
-
-Route::get('/reckh', 'PetitionController@index')->name('petitions.index');
-Route::post('/reckh', 'PetitionController@store')->name('petitions.store');
-Route::get('/reckh/vote', 'PetitionController@vote')->name('petitions.vote');
+Route::get('/reckh', [PetitionController::class, 'index'])->name('petitions.index');
+Route::get('/reckh/vote', [PetitionController::class, 'vote'])->name('petitions.vote');
+Route::post('/reckh', [PetitionController::class, 'store'])->name('petitions.store');
 
 Route::middleware(['auth'])->group(static function () {
-    Route::get('/home', 'HomeController@index')->name('home');
-
-    Route::delete('/message/{message}', 'MessageController@delete')->name('message.delete');
-    Route::put('/approve/{message}', 'MessageController@approve')->name('message.approve');
-    Route::post('/block', 'BlacklistController@store')->name('ip.store');
-    Route::delete('/block', 'BlacklistController@delete')->name('ip.delete');
+    Route::delete('/block', [BlacklistController::class, 'delete'])->name('ip.delete');
+    Route::delete('/message/{message}', [MessageController::class, 'delete'])->name('message.delete');
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+    Route::post('/block', [BlacklistController::class, 'store'])->name('ip.store');
+    Route::put('/approve/{message}', [MessageController::class, 'approve'])->name('message.approve');
 });
